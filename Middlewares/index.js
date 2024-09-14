a8118c863f376707cd02a6b08b36989b0600bf3c
Arshjeevs <harshan4648@gmail.com>
2024-09-14 14:00:11 +0530

    ADD: Added JWT to Middlewares

--- Middlewares/index.js
+++ Middlewares/index.js
@@ -1,19 +1,23 @@
-const JWT = require('jsonwebtoken');
-const JWT_SECRET = "TATHVA@NITC"
+const JWT = require("jsonwebtoken");
+const JWT_SECRET = "TATHVA@NITC";
 
-const authMiddleware = (req,res,next) => {
-    const token = req.headers.authorization;
-    if(!token || !token.startsWith('Bearer ')){
-        res.status(401).json({
-            message: "Invalid authorization"
-        })
-    }
-    const tokenSecret = token.split("")[1];
+const authMiddleware = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token || !token.startsWith("Bearer ")) {
+    res.status(401).json({
+      message: "Invalid authorization",
+    });
+  }
+  const tokenSecret = token?.split(" ")[1];
 
-    try{
-        const response = JWT.verify(tokenSecret, JWT_SECRET);
-        if(response.success){
-            console.log("HI")
-        }
-    }
-}
+  try {
+    const response = JWT.verify(tokenSecret, JWT_SECRET);
+    next();
+  } catch (err) {
+    res.status(403).json({
+      message: "Access Denied",
+    });
+  }
+};
+
+module.exports = authMiddleware;
--- Routes/User.js
+++ Routes/User.js
@@ -1,5 +1,41 @@
-const express = require('express');
+const express = require("express");
+const authMiddleware = require("../Middlewares");
+const JWT = require("jsonwebtoken");
+const JWT_SECRET = "TATHVA@NITC";
 
-const router = express.Router();   
+const router = express.Router();
 
-module.exports = router
+router.get("/read", authMiddleware, (req, res) => {
+  return res.json({ message: "User has read access" });
+});
+
+router.post("/create", (req, res) => {
+  const body = req.body;
+
+  const email = body.email;
+  const CreateToken = JWT.sign({ email }, JWT_SECRET);
+  return res.json({
+    message: "User created successfully",
+    CreateToken: CreateToken,
+  });
+});
+
+router.delete("/delete", authMiddleware, (req, res) => {
+    const body = req.body;
+    
+    const email = body.email;
+    const CreateToken = JWT.sign({ email }, JWT_SECRET);
+    return res.json({
+      message: "User created successfully",
+      CreateToken: CreateToken,
+    });
+  });
+
+  router.put("/update", authMiddleware, (req, res) => {
+    const body = req.body;
+    return res.json({
+        message: "User updated successfully",
+    })
+  });
+
+module.exports = router;
